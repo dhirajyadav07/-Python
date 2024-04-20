a0e420f19bd1a42600bc1b5ed5df9ceac013304b
Dhiraj Yadav <132497879+dhirajyadav07@users.noreply.github.com>
2024-04-20 15:53:13 +0530

    Add files via upload

---
+++ swaping_two_variable_value.py
@@ -0,0 +1,6 @@
+num1=int(input("first number: "))
+num2=int(input("second number: "))
+# now swaping these variable value 
+num1,num2=num2,num1
+print("first number",num1)
+print("second number",num2)
